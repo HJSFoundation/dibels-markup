861836204ff5fca287999f63618d18714e4ec3b1
Johnafriedman <john@innovationsforlearning.org>
2015-04-03 14:33:14 -0500

    add name from model to student tab

--- www/js/views/matrixStudentSelectorView.js
+++ www/js/views/matrixStudentSelectorView.js
@@ -1,20 +1,45 @@
+// App.Views.MatrixStudentSelector = Backbone.View.extend({
+//   template: App.templates.matrixStudentSelector,
+
+// config: {
+//     tabCount: 5
+//   },
+
+//   initialize: function() {
+//     _.bindAll(this);
+//     this.render();
+//     this.tabViews=[];
+//     for(var i=0;i<this.config.tabCount;i++){
+//       this.tabViews[i] = new App.Views.Tab({label: "tab", el: ".js-matrixStudentSelectorTabs"});
+//       this.$el.append(this.tabViews[i].el); 
+//     }
+//     // this.tabViews[0].makeActive();
+
+//   },
+
+//   render:  function() {
+//     this.$el.html(this.template());
+//   }
+// });
+
 App.Views.MatrixStudentSelector = Backbone.View.extend({
   template: App.templates.matrixStudentSelector,
 
-config: {
-    tabCount: 5
-  },
-
   initialize: function() {
     _.bindAll(this);
-    this.render();
-    this.tabViews=[];
-    for(var i=0;i<this.config.tabCount;i++){
-      this.tabViews[i] = new App.Views.Tab({el: ".js-matrixStudentSelectorTabs"});
-      this.$el.append(this.tabViews[i].el); 
-    }
-    // this.tabViews[0].makeActive();
 
+    var students = new App.Collections.Students();
+    students.fetch();
+    if(students.length===0){
+      students.create({firstname: "Bernie", lastname: "Bivins"});
+
+    }
+
+    this.render();
+    students.each(function(student){
+      var view = new App.Views.Tab({model: student, el: ".js-matrixStudentSelectorTabs"}); 
+      this.$el.append(view.el); 
+    });
   },
 
   render:  function() {
--- www/js/views/tabView.js
+++ www/js/views/tabView.js
@@ -1,13 +1,14 @@
 App.Views.Tab = Backbone.View.extend({
   template: App.templates.tab,
 
-  initialize: function() {
+  initialize: function(options) {
     _.bindAll(this);
+    this.model = options.model;
     this.render();
   },
 
   render:  function() {
-    this.$el.append(this.template());
+    this.$el.append(this.template({label: this.model.get("firstname")}));
   }
   // ,
 
