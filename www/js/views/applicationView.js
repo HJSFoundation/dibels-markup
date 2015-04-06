8cf2a280920de800e8bfc6c1dc9423bb5f2e7b33
Johnafriedman <john@innovationsforlearning.org>
2015-04-06 17:01:05 -0500

    adding student assessment data

--- www/js/views/applicationView.js
+++ www/js/views/applicationView.js
@@ -2,6 +2,35 @@ App.Views.Application = Backbone.View.extend({
   initialize: function() {
     this.loginView = new App.Views.Login({ el: this.$el });
     this.listenTo(App.Dispatcher, "loginSuccess", this.handleLoggedIn);
+  
+    App.students = new App.Collections.Students();
+    App.students.fetch();
+    if(App.students.length===0){
+      var letters = {};
+      var a="a".charCodeAt(0);
+      var z="z".charCodeAt(0);
+
+      for( var c=a; c<=z;c++){
+        letters[String.fromCharCode(c)]={assessment:"clear"};
+      }
+
+      var A="A".charCodeAt(0);
+      var Z="Z".charCodeAt(0);
+
+      for( var c=A; c<=Z;c++){
+        letters[String.fromCharCode(c)]={assessment:"clear"};
+      }
+
+      console.log(letters);
+      App.students.create({firstname: "Bernie", lastname: "Bivins", letters: letters});
+      App.students.create({firstname: "Matt", lastname: "Bivins", letters: letters});
+      App.students.create({firstname: "Evan", lastname: "Bivins", letters: letters});
+      App.students.create({firstname: "Clint", lastname: "Eastman", letters: letters});
+      App.students.create({firstname: "Hugo", lastname: "Bloch", letters: letters});
+
+    }
+
+
   },
 
   handleLoggedIn: function() {
--- www/js/views/matrixStudentSelectorView.js
+++ www/js/views/matrixStudentSelectorView.js
@@ -1,26 +1,4 @@
-// App.Views.MatrixStudentSelector = Backbone.View.extend({
-//   template: App.templates.matrixStudentSelector,
 
-// config: {
-//     tabCount: 5
-//   },
-
-//   initialize: function() {
-//     _.bindAll(this);
-//     this.render();
-//     this.tabViews=[];
-//     for(var i=0;i<this.config.tabCount;i++){
-//       this.tabViews[i] = new App.Views.Tab({label: "tab", el: ".js-matrixStudentSelectorTabs"});
-//       this.$el.append(this.tabViews[i].el); 
-//     }
-//     // this.tabViews[0].makeActive();
-
-//   },
-
-//   render:  function() {
-//     this.$el.html(this.template());
-//   }
-// });
 
 App.Views.MatrixStudentSelector = Backbone.View.extend({
   template: App.templates.matrixStudentSelector,
@@ -28,19 +6,8 @@ App.Views.MatrixStudentSelector = Backbone.View.extend({
   initialize: function() {
     _.bindAll(this);
 
-    var students = new App.Collections.Students();
-    students.fetch();
-    if(students.length===0){
-      students.create({firstname: "Bernie", lastname: "Bivins"});
-      students.create({firstname: "Matt", lastname: "Bivins"});
-      students.create({firstname: "Evan", lastname: "Bivins"});
-      students.create({firstname: "Clint", lastname: "Eastman"});
-      students.create({firstname: "Hugo", lastname: "Bloch"});
-
-    }
-
     this.render();
-    students.each(function(student){
+    App.students.each(function(student){
       var view = new App.Views.Tab({model: student, el: ".js-matrixStudentSelectorTabs"}); 
     });
   },
