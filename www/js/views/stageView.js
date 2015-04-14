6c2aa018b14c06a46b099f8e049948076e1209b0
Johnafriedman <john@innovationsforlearning.org>
2015-04-14 14:05:12 -0500

    adding matrix toggle handling for stage

--- www/js/views/stageView.js
+++ www/js/views/stageView.js
@@ -15,13 +15,19 @@ App.Views.Stage = Backbone.View.extend({
     this.buttonTimerView = new App.Views.ButtonTimer({el: ".js-stageButtonTimer"});
     this.menuAssessmentView = new App.Views.MenuAssessment({ el: ".js-menuAssessment"});
     this.menuActivityView = new App.Views.MenuActivity({ el: ".js-menuActivity"});
+    this.listen();
   },
 
   listen: function(){
-    this.listenTo(App.Dispatcher, "changeSkillRequest", this.handlechangeSkillRequest);
+    this.listenTo(App.Dispatcher, "closeMatrix", this.handleCloseMatrix);
   },
 
   render: function() {
     this.$el.html(this.template());
   },
+
+  handleCloseMatrix: function (){
+    this.$el.addClass("animated slideOutDown");
+  }
+  
 });
