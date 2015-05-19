6d66c172d3e4bcc769c187bad872a52e10896436
Johnafriedman <john@innovationsforlearning.org>
2015-05-19 15:50:08 -0500

    removing flickity from stage stimulus letters

--- www/js/views/stageStimulusLettersView.js
+++ www/js/views/stageStimulusLettersView.js
@@ -4,18 +4,6 @@ App.Views.StageStimulusLetters = Backbone.View.extend({
   initialize: function() {
     _.bindAll(this);
     this.listen();
-    this.alphabet=[];
-    var a="a".charCodeAt(0);
-    var z="z".charCodeAt(0);
-    var c;
-    for( c = a; c <= z; c = c + 1) {
-      this.alphabet.push[String.fromCharCode(c)];
-    }
-    a="A".charCodeAt(0);
-    z="Z".charCodeAt(0);
-    for( c = a; c <= z; c = c + 1) {
-      this.alphabet.push[String.fromCharCode(c)];
-    }
   },
 
   listen: function() {
@@ -24,14 +12,6 @@ App.Views.StageStimulusLetters = Backbone.View.extend({
 
   render: function(stimulus_object) {
     this.$el.html(this.template(stimulus_object));
-    $('.stage__stimulus__gallery').flickity({
-      // options
-      cellAlign: 'left',
-      contain: true,
-      prevNextButtons: false,
-      pageDots: false
-    });
-    
   },
 
   handleSkillChangeRequest: function(stimulus_object) {
