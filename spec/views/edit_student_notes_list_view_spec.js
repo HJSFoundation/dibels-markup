556df14ddf3682289b0254899010b185d1aac38c
Johnafriedman <john@innovationsforlearning.org>
2015-05-28 08:59:52 -0500

    adding tests for edit student notes

--- spec/views/edit_student_notes_list_view_spec.js
+++ spec/views/edit_student_notes_list_view_spec.js
@@ -44,28 +44,43 @@ describe('App.Views.EditStudentNotesList', function() {
   
 
   describe("#render", function() {
-    xit("renders", function() {
+    it("renders", function() {
       subject.render();
       expect(subject.$el).not.to.be.empty;
     });
   });
 
   describe("handlers", function() {
-    xit("#handleEditStudentNoteSelected", function() {
-      subject.render();
-      sinon.spy(subject.article, "render");
-      subject.handleEditStudentNoteSelected(new App.Views.EditStudentNote({model: new App.Models.Note}));
-      expect(subject.article.render).to.have.been.called;
-    });
+    describe("#handleEditStudentNoteSelected", function() {
+      it(" calls makeInactive", function() {
+        sinon.spy(subject, "makeInactive");
+        subject.handleEditStudentNoteSelected(new App.Views.EditStudentNote({model: new App.Models.Note}));
+        expect(subject.makeInactive).to.have.been.called;
+      });
+
+      it(" calls makeActive", function() {
+        sinon.spy(subject, "makeActive");
+        var noteView = new App.Views.EditStudentNote({model: new App.Models.Note});
+        subject.handleEditStudentNoteSelected(noteView);
+        expect(subject.makeActive).to.have.been.calledWith(noteView);
+      });
+    }); 
   });
 
   describe("helpers", function() {
-    xit("#makeInactive", function() {
-      
+    it("#makeInactive", function() {
+      var noteView = new App.Views.EditStudentNote({model: new App.Models.Note});
+      subject.noteViews.push(noteView);
+      sinon.spy(noteView, "makeInactive");
+      subject.makeInactive();
+      expect(noteView.makeInactive).to.have.been.called;
     });
 
-    xit("makeActive", function() {
-      
+    it("makeActive", function() {
+      var noteView = new App.Views.EditStudentNote({model: new App.Models.Note});
+      sinon.spy(noteView, "makeActive");
+      subject.makeActive(noteView);
+      expect(noteView.makeActive).to.have.been.called;
     });
   });
 });
