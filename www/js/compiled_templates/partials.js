b515591860bf8b00a0a8bc8eb058b87ea070adfb
Johnafriedman <john@innovationsforlearning.org>
2015-06-02 10:26:42 -0500

    fixing fail on CodeShip

--- www/js/compiled_templates/partials.js
+++
@@ -1,3 +0,0 @@
-Handlebars.registerPartial("buttonStop", Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
-    return "<button class=\"button--warning button--stop\">stop</button>\n";
-},"useData":true}));
