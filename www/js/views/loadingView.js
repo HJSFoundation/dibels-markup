6c90b5496b8c5c02868f8a9b4327649271cb9037
Matthew Bivins <matt@bivinsbrothers.com>
2015-06-22 15:26:14 -0500

    loading page

--- www/js/views/loadingView.js
+++ www/js/views/loadingView.js
@@ -8,6 +8,27 @@ App.Views.Loading = Backbone.View.extend({
 
   render: function() {
     this.$el.html(this.template());
+
+    // THE PROGRESS BAR
+    var progressbar = $('#progress-bar'),
+      max = progressbar.attr('max'),
+      value = progressbar.val(),
+      time = (1000/max)*5;
+
+    var loading = function() {
+      value += 1;
+      addValue = progressbar.val(value);
+
+      // $('.progress-value').html(value + '%');
+
+      if (value == max) {
+        clearInterval(animate);
+      }
+    }
+
+    var animate = setInterval(function() {
+      loading();
+    }, time);
   },
 
   removeView: function() {
