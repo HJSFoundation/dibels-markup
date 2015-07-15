9dbf6c4a7fcfadfa01d6def74a55254aee656b3a
Johnafriedman <john@innovationsforlearning.org>
2015-07-15 12:20:21 -0500

    adding nacMain; tweaking styles

--- www/js/views/navMainView.js
+++ www/js/views/navMainView.js
@@ -2,7 +2,9 @@ App.Views.NavMain = Backbone.View.extend({
   template: App.templates.navMain,
 
   events: {
-    'click': 'handleToggleMenu'
+    'click .js-menuToggle': 'handleToggleMenu',
+    'click .js-manageButton': 'handleDisplayManage',
+    'click .js-logout' : 'handleLogout'
   },
 
   initialize: function() {
@@ -16,5 +18,17 @@ App.Views.NavMain = Backbone.View.extend({
 
   handleToggleMenu: function() {
     $(".js-mainNav").toggleClass("st-show");
+  },
+
+  handleDisplayManage: function() {
+    App.browser = window.open(App.Config.tutormateUrl + "/students/"+App.currentTeacher.classroom_id, "_blank", "location=yes");
+
+    console.log("handleDisplayManage");
+    return false;
+  },
+
+  handleLogout: function(){
+    App.Dispatcher.trigger("logout");
   }
+
 });
