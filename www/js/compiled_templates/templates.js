93534005cce59c297b2c3a1270597b24bb703d0a
Matthew Bivins <matt@bivinsbrothers.com>
2015-08-25 16:50:18 -0500

    style work on story/featured text flip button & spacing

--- www/js/compiled_templates/templates.js
+++ www/js/compiled_templates/templates.js
@@ -252,7 +252,7 @@ this["App"]["templates"]["leveledTextPage"] = Handlebars.template({"1":function(
 
   return "<div class=\"story container\" style=\"position:absolute;top:0px;left:0px;width:100%;height:100%;z-index:100;background-color: white\">\n  <div class=\"stage--story stage\">\n    <div class=\"stage--story__gallery\">\n"
     + ((stack1 = helpers.each.call(depth0,(depth0 != null ? depth0.pages : depth0),{"name":"each","hash":{},"fn":this.program(1, data, 0, blockParams, depths),"inverse":this.noop,"data":data})) != null ? stack1 : "")
-    + "    </div>\n    <footer class=\"story__footer\">\n      <button class=\"story__flip js-storyButtonFlip\"></button>\n      <button class=\"button button--close js-storyButtonCloseStory\"></button>\n    </footer>\n    <div class=\"js-readingStrategies\"></div>\n  </div>\n</div>";
+    + "    </div>\n      <button class=\"story__flip js-storyButtonFlip\"></button>\n      <button class=\"button button--close js-storyButtonCloseStory\"></button>\n    <div class=\"js-readingStrategies\"></div>\n  </div>\n</div>";
 },"useData":true,"useDepths":true});
 this["App"]["templates"]["loading"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
     var helper;
@@ -429,7 +429,7 @@ this["App"]["templates"]["stageStoryPage"] = Handlebars.template({"1":function(d
 
   return "<div class=\"story container\" style=\"position:absolute;top:0px;left:0px;width:100%;height:100%;z-index:100;background-color: white\">\n  <div class=\"stage--story stage\">\n    <div class=\"stage--story__gallery\">\n"
     + ((stack1 = helpers.each.call(depth0,(depth0 != null ? depth0.pages : depth0),{"name":"each","hash":{},"fn":this.program(1, data, 0, blockParams, depths),"inverse":this.noop,"data":data})) != null ? stack1 : "")
-    + "    </div>\n    <footer class=\"story__footer\">\n      <button class=\"story__flip js-storyButtonFlip\"></button>\n      <button class=\"button button--close js-storyButtonCloseStory\"></button>\n    </footer>\n    <div class=\"js-readingStrategies\"></div>\n  </div>\n</div>";
+    + "    </div>\n    <button class=\"story__flip js-storyButtonFlip\"></button>\n    <button class=\"button button--close js-storyButtonCloseStory\"></button>\n    <div class=\"js-readingStrategies\"></div>\n  </div>\n</div>";
 },"useData":true,"useDepths":true});
 this["App"]["templates"]["stimuliTilesLetters"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
     var helper;
